25a3fa85c04e6c2cb887793c59d56c0081775b32
Bobo-peng <zhangjiawan610@163.com>
2019-01-07 17:24:14 +0800

    加注释

--- src/0042TrappingRainWater/42TrappingRainWater.cpp
+++ src/0042TrappingRainWater/42TrappingRainWater.cpp
@@ -8,12 +8,14 @@ int trap(vector<int>& height) {
 	int n = height.size();
 	int res = 0, max = 0;
 	vector<int> mem(n, 0);
+	//从左到右，记录i之前的最大值
 	for (int i = 0; i < n; ++i)
 	{
 		mem[i] = max;
 		max = height[i] > max ? height[i] : max;	
 	}
 	max = 0;
+	//从右到左，记录i后面最大值，然后和mem[i]中的值比较，取小的值；然后i位置上的值和mem[i]差值就是i位置上存的雨水量。
 	for (int i = n - 1; i >= 0; --i)
 	{
 		mem[i] = mem[i] < max ? mem[i] : max;
